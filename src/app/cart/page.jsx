c77047021ce81d39475f2137e5c19c319b67be06
664Parvez <parvezrabbi664@gmail.com>
2025-05-20 00:01:21 +0600

    update

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+"use client"
+
+import React, {useState} from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import CartCss from "../styles/cart.module.css"
 
 // Component
@@ -7,9 +10,17 @@ import Sidegap from '../../components/sidegap'
 
 // Icons
 import { FaArrowRightLong } from "react-icons/fa6";
+import { IoTrashBin } from "react-icons/io5";
+
+// Image
+import Product1 from "../../../public/image/product1.jpg"
 
 
 const Cart = () => {
+
+  const [counter, setCounter] = useState(1)
+
+
   return (
     <>
         <Sidegap>
@@ -22,24 +33,58 @@ const Cart = () => {
               <form action="">
                 <div className="row">
 
-                  <div className="col-lg-8 col-md-6">
+                  <div className="col-lg-8 col-md-12">
                     <div className={CartCss.checkoutLeft}>
-                      <div class="table-responsive">
-                        <table class="table align-middle">
+                      <div className="table-responsive">
+                        <table className="table align-middle">
                           <thead>
                             <tr>
                               <th>Product</th>
+                              <th>Price</th>
                               <th>Quantity</th>
                               <th>Total</th>
+                              <th></th>
                             </tr>
                           </thead>
-                          <tbody></tbody>
+                          <tbody>
+                            <tr>
+                              <td>
+                                <div className="d-flex justify-content-start align-items-center gap-3">
+                                  <div>
+                                    <Link href="">
+                                      <Image src={Product1} layout='responsive' className='rounded' height={0} width={0} alt=''></Image>
+                                    </Link>
+                                  </div>
+                                  <div>
+                                    <h5 className='mb-0'>Faux-leather trousers</h5>
+                                    <p>Color: Black</p>
+                                  </div>
+                                </div>
+                              </td>
+                              <td><span>$125</span></td>
+                              <td>
+                                <div className='d-flex justify-content-start align-items-center gap-3'>
+                                  <div>
+                                    <span className={CartCss.incDecBtn} onClick={() => setCounter(counter + 1)}>+</span>
+                                  </div>
+                                  <div>
+                                    <input type="text" className="form-control form-control-lg text-center" defaultValue={counter} />
+                                  </div>
+                                  <div>
+                                    <span className={CartCss.incDecBtn} onClick={() => counter <= 1 ? setCounter(1) : setCounter(counter - 1)}>-</span>
+                                  </div>
+                                </div>
+                              </td>
+                              <td><span><b>$125</b></span></td>
+                              <td className='text-end'><button className='btn btn-lg'><IoTrashBin /></button></td>
+                            </tr>
+                          </tbody>
                         </table>
                       </div>
                     </div>
                   </div>
 
-                  <div className="col-lg-4 col-md-6">
+                  <div className="col-lg-4 col-md-12">
                     <div className={CartCss.checkoutRight}>
                       <h2>Order Summary</h2>
 
